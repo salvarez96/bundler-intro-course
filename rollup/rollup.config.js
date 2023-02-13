4f3a0b7f1a8e87b9a1aa5b82f79c531ff70c2dc6
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-02-12 22:17:05 -0500

    Class 19: add rollup plugins in rollup.config.js

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,4 +1,8 @@
+import html from "@rollup/plugin-html";
+import image from "@rollup/plugin-image";
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
+import css from "rollup-plugin-css-only";
 
 export default {
 	input: 'src/index.ts',
@@ -7,5 +11,20 @@ export default {
 		format: 'es',
     sourcemap: true
 	},
-  plugins: [typescript()]
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: "src/assets/**/*",
+          dest: "dist/assets"
+        }
+      ]
+    }),
+    css({
+      output: "bundle.css"
+    }),
+    typescript(),
+    html(),
+    image(),
+  ]
 };
